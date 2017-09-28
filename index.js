e9972e4adc1e0876447c81452e9cc5a3eabb7260
Vitor Augusto <vitor_augusto@id.uff.br>
2017-09-27 22:21:24 -0300

    teste9

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on('unknownCommand', message => {
     console.log("Available: ", message.guild.available);
     if (message.guild.available){
         emoji = message.guild.emojis.random();
-        message.reply("Wubbadubbahuphup ${emoji}");
+        message.reply('Wubbadubbahuphup ${emoji}');
     }
     // Send an emoji:
     // const emoji = guild.emojis.first();
