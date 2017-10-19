a158bdaf2e90d52cafef93707ffec4c131bd9c0f
Vitor Augusto <vitor_augusto@id.uff.br>
2017-10-19 18:02:32 -0200

    play teste2

--- index.js
+++ index.js
@@ -26,9 +26,6 @@ bot.on('ready', () => {
 bot.on('unknownCommand', message => {
     console.log('EUERKA!'); //TODO remove logs and check if emojis collection is not empty
     console.log("Available: ", message.guild.available);
-    if (message.content == "alo") {
-        message.say("Alô @Vitorgus !");
-    }
     if (message.content == bot.commandPrefix + "activate eggplant mode"){
         eggplant = true;
         console.log("Eggplant mode activated!");
@@ -49,6 +46,9 @@ bot.on('unknownCommand', message => {
 });
 
 bot.on('message', message => {
+    if (message.content == "alo") {
+        message.say("Alô @Vitorgus !");
+    }
     if (eggplant && message.author.username == "Zorg"){
         message.react("🍆");
     }
