a7da13b13f1926145bddc9fffb482f42a565e532
Vitor Augusto <vitor_augusto@id.uff.br>
2017-12-28 18:49:29 -0200

    trying to change toaster game

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on('ready', () => {
     console.log("type "+bot.commandPrefix+"help in Discord for a commands list.");
     bot.user.setStatus("online");
     //bot.user.setGame("JARVIS | jarvis help");
-    bot.user.setGame("Type 'jarvis help' for a commands list");
+    bot.user.setGame("Type 'jarvis help' for commands");
 });
 
 bot.on('unknownCommand', message => {
