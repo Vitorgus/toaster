8e3d29355619a90ce9b53c44085bbd9bd7797774
Vitor Augusto <vitor_augusto@id.uff.br>
2017-12-28 18:33:38 -0200

    little patch

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on('ready', () => {
     console.log("type "+bot.commandPrefix+"help in Discord for a commands list.");
     bot.user.setStatus("online");
     //bot.user.setGame("JARVIS | jarvis help");
-    bot.user.setGame("For commands, type 'jarvis help'");
+    bot.user.setGame("Type 'jarvis help' for a list of commands");
 });
 
 bot.on('unknownCommand', message => {
