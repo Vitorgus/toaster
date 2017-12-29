62e85f3d42386e1f45e56ce93ea21c355633c6a8
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2017-12-28 22:06:52 -0200

    commented index.js

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
 const path = require('path');
 const Commando = require('discord.js-commando');
-//const token = require('./token.js');
 const token = process.env.TOKEN;
+
+//Initializing bot
 const bot = new Commando.Client({
-    owner: '291235973717688321',
-    commandPrefix: 'jarvis ',
-    disableEveryone: true,
-    unknownCommandResponse: false
+    owner: '291235973717688321',    // Setting myself as the owner. That's my Discord ID.
+    commandPrefix: 'jarvis ',       // Setting the prefix
+    disableEveryone: true,          // Uhh... not sure what this does
+    unknownCommandResponse: false   // Disable the default unknown command response, So that it can reply with a random custom emoji later on the code
 });
 
-var eggplant = false;
+var eggplant = false;   //Variable that keeps track if it is to eggplant Zorg or not
 
+
+/*
+Code that will be executed when the bot is initialized.
+It mostly just set some things and logs that the bot is online
+*/
 bot.on('ready', () => {
-    //console.log('I\'m doing things...');
-    //bot.user.setAvatar('http://i.imgur.com/l2KqI3Y.png?1');
-    bot.user.setAvatar('http://www.jeffbots.com/hal.jpg');
-    package = require('./package.json');
-    console.log("Starting " + package.name + " " + package.version + "...\nLogged in! I'm now hosted on GitHub!");
-    console.log("type "+bot.commandPrefix+"help in Discord for a commands list.");
-    bot.user.setStatus("online");
+    bot.user.setAvatar('http://www.jeffbots.com/hal.jpg');                                  // Sets the avatar image
+    package = require('./package.json');                                                    // Gets the package.json file
+    console.log("Starting " + package.name + " " + package.version + "...\nLogged in!");    // Outputs in the log that the bot has started
+    console.log("type "+bot.commandPrefix+"help in Discord for a commands list.");          // Same as above
+    bot.user.setStatus("online");                                                           // Sets bot status
     //bot.user.setGame("JARVIS | jarvis help");
-    bot.user.setGame("Type 'jarvis help' for commands");
+    bot.user.setGame("Type 'jarvis help' for commands");                                    // Sets bot game
 });
 
+/*
+Code that will be executed when jarvis gets a command that doesn't exists.
+First, it will check if it is one of the commands to activate or deactivate eggplant mode.
+If not, he will reply with a random custom emoji.
+NOTE: If there's no custom emoji, he does nothing.
+*/
 bot.on('unknownCommand', message => {
     //TODO check if emojis collection is not empty
-    if (message.content == bot.commandPrefix + "activate eggplant mode"){
-        eggplant = true;
-        console.log("Eggplant mode activated!");
-        message.say("Activated 🍆");
+    if (message.content == bot.commandPrefix + "activate eggplant mode"){   // Check if it is the command to activate the eggplant
+        eggplant = true;                                                    // Activates
+        console.log("Eggplant mode activated!");                            // Logs it
+        message.say("Activated 🍆");                                         // Sends a message confirming the activation
     }
-    else if (message.content == bot.commandPrefix + "deactivate eggplant mode"){
-        if (message.author.username == "Zorg"){
-            message.say(message.guild.emojis.find("name", "cliffsmug").toString());
-            return;
-        }
-        eggplant = false;
-        console.log("Eggplant mode deactivated!");
-        message.say("Deactivated 🍆");
+    else if (message.content == bot.commandPrefix + "deactivate eggplant mode"){        // Check if it is the command to deactivate the eggplant
+        if (message.author.username == "Zorg"){                                         // If Zorg is trying to theactiate the eggplant
+            message.say(message.guild.emojis.find("name", "cliffsmug").toString());     // Respond with a cliffsmug
+            return;                                                                     // Exits the code
+        }                                                                               // If it wasn't Zorg, then the code continues
+        eggplant = false;                                                               // Deactivates
+        console.log("Eggplant mode deactivated!");                                      // Logs it
+        message.say("Deactivated 🍆");                                                   // Sends a message confirming it
+    }                                               // If it wasn't any of the two commands, then...
+    else if (message.guild.available){              // Checks if it is able to send
+        emoji = message.guild.emojis.random();      // Gets a random custom emoji
+        message.say(emoji.toString());              // Says the emoji in the chat
     }
-    else if (message.guild.available){
-        emoji = message.guild.emojis.random();
-        message.say(emoji.toString());
-    }
+    /* THIS CODE IS JUST HERE TO REMIND ME THAT THE FOLOWING IS POSSIBLE
     // Send an emoji:
-    // const emoji = guild.emojis.first();
-    // msg.reply(`Hello! ${emoji}`);
+    const emoji = guild.emojis.first();
+    msg.reply(`Hello! ${emoji}`);
+    */
 });
 
+
+/*
+Coda that will be executed when a message is send.
+It happens for every message that appears in a server the bot is in.
+This just serves to eggplant zorg if the eggplant mode is active.
+*/
 bot.on('message', message => {
-    /*
+    /* THIS CODE IS JUST HERE TO REMIND ME THAT THE FOLOWING IS POSSIBLE
     if (message.content == "alo") {
         message.channel.send("<@291235973717688321><:red:362768065202618369>");
     }*/
-    if (eggplant && message.author.username == "Zorg"){
-        message.react("🍆");
+    if (!eggplant) return;                      // If the mode is deactivated, stop
+    if (message.author.username == "Zorg"){     // If it gets here, then it is activated, so it checks if the sender os the message is Zorg
+        message.react("🍆");                     // If yes, the reacts with an eggplant
     }
 });
 
-process.on('unhandledRejection', console.error);
+process.on('unhandledRejection', console.error);    // ...I guess this line is important, but I don't know why
 
+// Registers the commands for the bot and divide them in their categories
 bot.registry
     .registerDefaultTypes()
     .registerGroups([
@@ -74,5 +94,5 @@ bot.registry
     .registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-//Login
+//Login into discord. WHy is this line in the bottom of the code?
 bot.login(token);
