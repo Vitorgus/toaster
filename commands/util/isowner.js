44fefd390a6531608c4b556c2505175f9a00609a
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2017-12-29 13:56:12 -0200

    isowner.js now indented with 4 spaces

--- commands/util/isowner.js
+++ commands/util/isowner.js
@@ -1,21 +1,21 @@
 const { Command } = require('discord.js-commando');
 
 module.exports = class isownerCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'isowner',
-      group: 'util',
-      memberName: 'isowner',
-      description: 'debug function to verify if the user is the owner of the bot',
-      examples: ['jarvis isowner'],
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'isowner',
+            group: 'util',
+            memberName: 'isowner',
+            description: 'debug function to verify if the user is the owner of the bot',
+            examples: ['jarvis isowner'],
+        });
+    }
 
-  run(msg) {
-  	var bool = this.client.isOwner(msg.author);
-  	if (bool)
-    	return msg.say("Hello, Vitorgus! You are my owner!");
-    else
-      return msg.say("Sorry, but you are not my owner.");
-  }
+    run(msg) {
+        var bool = this.client.isOwner(msg.author);
+        if (bool)
+            return msg.say("Hello, Vitorgus! You are my owner!");
+        else
+            return msg.say("Sorry, but you are not my owner.");
+    }
 };
