bb0404bbfaa0a0b64bc0ca65771574d57cf8f6c9
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2017-12-29 11:59:46 -0200

    unknowncommand teste9

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ bot.on('unknownCommand', message => {
     //TODO check if emojis collection is not empty
     //TODO eggplant command with emap
     //console.info(Array.from(message.guild.emojis.values()));
-    console.info(message.guild.emojis.length);
+    console.info(Object.keys(message.guild.emojis).length);
     if (message.guild.emojis)
         console.info("There's custom emojis!");
     else
