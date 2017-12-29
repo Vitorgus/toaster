1f9e4d1fca988bcc4e7f943ca2cf7a2633896066
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2017-12-28 22:18:24 -0200

    added a simple TODO

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ NOTE: If there's no custom emoji, he does nothing.
 */
 bot.on('unknownCommand', message => {
     //TODO check if emojis collection is not empty
+    //TODO eggplant command with emap
     if (message.content == bot.commandPrefix + "activate eggplant mode"){   // Check if it is the command to activate the eggplant
         eggplant = true;                                                    // Activates
         console.log("Eggplant mode activated!");                            // Logs it
