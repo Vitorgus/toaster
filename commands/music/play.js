dea33ef89f0784b4a3ef9513948348151eb1edec
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-02-19 11:42:13 -0300

    play command teste3

--- commands/music/play.js
+++ commands/music/play.js
@@ -79,8 +79,13 @@ module.exports = class playCommand extends Command {
             voiceChannel.join()
             .then(connnection => {
                 console.log(final_url);
+                console.log("Why?????");
                 const stream = yt(final_url, {filter: 'audioonly'});
+                console.log("Stream:");
+                console.log(stream);
                 const dispatcher = connnection.playStream(stream);
+                console.log("Dispatcher:");
+                console.log(dispatcher);
                 dispatcher.on('end', () => {
                     console.log("dispatcher ended");
                     voiceChannel.leave();
