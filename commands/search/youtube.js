2eb11273520d0196b72b6a89d5b723aff9c45885
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-03-16 16:22:40 -0300

    youtube command updated

--- commands/search/youtube.js
+++ commands/search/youtube.js
@@ -6,34 +6,34 @@ youtube.setKey(process.env.YOUTUBE_KEY);
 youtube.addParam('type', 'video');
 
 module.exports = class youtubeCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'youtube',
-      group: 'search',
-      memberName: 'youtube',
-      description: 'gets youtube video matching the tags stated by the user',
-      examples: [';youtube'],
-      args: [{
-        key: 'tags',
-        prompt: 'What tags would you like to search for?',
-        type: 'string'
-      }]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'youtube',
+            group: 'search',
+            memberName: 'youtube',
+            description: 'gets youtube video matching the tags stated by the user',
+            examples: [';youtube'],
+            args: [{
+                key: 'tags',
+                prompt: 'What tags would you like to search for?',
+                type: 'string'
+            }]
+        });
+    }
 
-  run(msg, args) {
-    const { tags } = args;
-    youtube.search(tags, 1, function(error, result) {
-        if (error) {
-          msg.say("¯\\_(ツ)_/¯");
-        }
-        else {
-          if (!result || !result.items || result.items.length < 1) {
-            msg.say("¯\\_(ツ)_/¯");
-          } else {
-            msg.say("http://www.youtube.com/watch?v=" + result.items[0].id.videoId );
-          }
-        }
-      });
-  }
+    run(msg, { tags }) {
+        youtube.search(tags, 1, {type: "video"}, function(error, result) {
+            if (error)  return msg.say(`Error while searching for the video:  \`${error}\``);
+
+            if (!result || !result.items || result.items.length < 1) {
+                console.log("ERROR_PLAY2");
+                console.log("Result: " + result);
+                console.log("Result items: " + result.items);
+                console.log("Result items length " + result.items.length);
+                return msg.say(`Something that couldn't go wrong, went wrong. ${anchor.client.owners[0]}, check the logs.`);
+            }
+            else msg.say("http://www.youtube.com/watch?v=" + result.items[0].id.videoId);
+        });
+
+    }
 };
