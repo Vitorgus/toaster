8b19616444f29934aa2a2240cd6938895489606b
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-03-18 19:09:03 -0300

    > changed to >= (I am stupid)

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -17,7 +17,7 @@ module.exports = class stopCommand extends Command {
         if(!msg.guild.voiceConnection) return msg.say("I'm not connected to a Voice Channel!");
         if (!music || !music.playing) return msg.say("No music is playing!");
         if (!music.queue.length) return msg.say("There's no music in the queue!");
-        for (let i = music.queue.length - 1; i > 0; i--) {
+        for (let i = music.queue.length - 1; i >= 0; i--) {
             console.log("Loop: " + i);
             if (music.queue[i].user === msg.author.id) {
                 console.log("Found!");
