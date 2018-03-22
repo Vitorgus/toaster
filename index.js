a05ac76b15964cc548de906bfb6ae808b83ba0a6
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-03-22 18:33:05 -0300

    Forgot to capitalize an i

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ function checkStream(offline) {
                         .send("Stream is on! https://picarto.tv/REDnFLYNN");
                     return;
                 }
-                clearinterval(bot.stream_timer);
+                clearInterval(bot.stream_timer);
                 setTimeout(checkStream.bind(null, true), 60000); //300000
                 //console.log(rnf);
             } catch (e) {
