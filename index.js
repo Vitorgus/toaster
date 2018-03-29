116abf7f1ab4c97b9fd01865dddedb7cc985f5c5
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-03-29 18:48:18 -0300

    beforeExit and exit did nothing

--- index.js
+++ index.js
@@ -121,14 +121,6 @@ process.on('unhandledRejection', (reason, p) => {               // ...I guess th
   console.log('Unhandled Rejection at:', p, 'reason:', reason);
 });
 
-process.on('beforeExit', (code) => {
-  console.log(`Before exit event with code: ${code}`);
-});
-
-process.on('exit', (code) => {
-  console.log(`About to exit with code: ${code}`);
-});
-
 // Registers the commands for the bot and divide them in their categories
 bot.registry
     .registerDefaultTypes()
