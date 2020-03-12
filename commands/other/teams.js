8ae421ee5e57787fea00c90b09f251b8d5ec9b48
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-03-12 19:27:41 -0300

    teams.js: one variable renamed and more logs added

--- commands/other/teams.js
+++ commands/other/teams.js
@@ -19,14 +19,16 @@ module.exports = class teamsCommand extends Command {
 
        roles_array.forEach(object => {
            let role = this.client.guilds.get(process.env.SHILOH_SERVER).roles.get(object.id);
-           if (!role || role.deleted) return;
-           teams_size.push({ name: role.name, members: role.members.size });
+           //if (!role || role.deleted) return;
+           if (!role) console.log("Coudn't find team with id " + object.id);
+           else if (role.deleted) console.log(`Role ${role.name} with id ${object.id} is deleted, skipping`);
+           else teams_size.push({ name: role.name, size: role.members.size });
        });
 
        if (teams_size.length === 0) return msg.say("Something went wrong. No roles found.");
 
-       teams_size.sort((a, b) => a.members > b.members ? -1 : a.members == b.members ? 0 : 1)
-       let answer = teams_size.reduce((string, role) => string + role.name + ' - ' + role.members + '\n', 'Number of members in each team:\n\n');
+       teams_size.sort((a, b) => a.size > b.size ? -1 : a.size == b.size ? 0 : 1)
+       let answer = teams_size.reduce((string, role) => string + role.name + ' - ' + role.size + '\n', 'Number of members in each team:\n\n');
        return msg.say(answer);
     }
 };
