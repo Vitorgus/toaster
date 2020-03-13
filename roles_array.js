a6e7933c5d675e8f0c9de3ad9eac8c97105aa386
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-03-12 22:44:56 -0300

    Removed description property and added more names to the roles

--- roles_array.js
+++ roles_array.js
@@ -1,52 +1,42 @@
 module.exports = [
     {
-        names: ['team ollie'],
-        id: process.env.TEAM_OLLIE,
-        description: ''
+        names: ['team ollie', 'team mantis', 'olive garden'],
+        id: process.env.TEAM_OLLIE
     },
     {
-        names: ['team sawyer'],
-        id: process.env.TEAM_SAWYER,
-        description: ''
+        names: ['team sawyer', 'team hot dad', 'choking hazard'],
+        id: process.env.TEAM_SAWYER
     },
     {
-        names: ['team calahan'],
-        id: process.env.TEAM_CALLAHAN,
-        description: ''
+        names: ['team calahan', 'team chloe', 'team best girl'],
+        id: process.env.TEAM_CALLAHAN
     },
     {
-        names: ['team barret'],
-        id: process.env.TEAM_BARRET,
-        description: ''
+        names: ['team barret', 'team cliff', 'snark tank'],
+        id: process.env.TEAM_BARRET
     },
     {
-        names: ['team sid'],
-        id: process.env.TEAM_SID,
-        description: ''
+        names: ['team sid', 'siddie pool'],
+        id: process.env.TEAM_SID
     },
     {
         names: ['team shane'],
-        id: process.env.TEAM_SHANE,
-        description: ''
+        id: process.env.TEAM_SHANE
     },
     {
         names: ['team hollis'],
-        id: process.env.TEAM_HOLLIS,
-        description: ''
+        id: process.env.TEAM_HOLLIS
     },
     {
-        names: ['team highwayman'],
-        id: process.env.TEAM_HIGHWAYMAN,
-        description: ''
+        names: ['team highwayman', 'team hwm'],
+        id: process.env.TEAM_HIGHWAYMAN
     },
     {
-        names: ['team grant'],
-        id: process.env.TEAM_GRANT,
-        description: ''
+        names: ['team grant', 'team henry'],
+        id: process.env.TEAM_GRANT
     },
     {
-        names: ['team todd'],
-        id: process.env.TEAM_TODD,
-        description: ''
+        names: ['team todd', 'team ainsley'],
+        id: process.env.TEAM_TODD
     }
 ];
