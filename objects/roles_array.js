2282985748fb4c7ea633377465b61fc2301d95d2
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-09-30 22:48:08 -0300

    Ollie is now an emo princess

--- objects/roles_array.js
+++ objects/roles_array.js
@@ -1,6 +1,6 @@
 module.exports = [
     {
-        names: ['ollie', 'thotlie', 'mantis', 'olive garden', 'oliver van mantis', 'van mantis'],
+        names: ['ollie', 'thotlie', 'mantis', 'olive garden', 'oliver van mantis', 'van mantis', 'emo princess'],
         id: process.env.SHILOH_ROLE_TEAM_OLLIE
     },
     {
