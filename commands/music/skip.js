4e192858dc74b9858a2f3ce69bab3f8e308da97c
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-10-30 20:00:50 -0300

    Commented music commands

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,25 +1,25 @@
-const { Command } = require('discord.js-commando');
+// const { Command } = require('discord.js-commando');
 
-module.exports = class skipCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'skip',
-            aliases: [
-                'next'
-            ],
-            group: 'music',
-            memberName: 'skip',
-            description: 'Skips the current song',
-            examples: ['jarvis skip', 'jarvis next'],
-            guildOnly: true
-        });
-    }
+// module.exports = class skipCommand extends Command {
+//     constructor(client) {
+//         super(client, {
+//             name: 'skip',
+//             aliases: [
+//                 'next'
+//             ],
+//             group: 'music',
+//             memberName: 'skip',
+//             description: 'Skips the current song',
+//             examples: ['jarvis skip', 'jarvis next'],
+//             guildOnly: true
+//         });
+//     }
 
-    run(msg, args) {
-        let music = this.client.music[msg.guild.id];
-        if(!msg.guild.voiceConnection) return msg.say("I'm not connected to a Voice Channel!");
-        if (!music || !music.playing) return msg.say("No music is playing!");
-        msg.guild.voiceConnection.dispatcher.end("Skipped!");
-        //console.log('Skipped!');
-    }
-};
+//     run(msg, args) {
+//         let music = this.client.music[msg.guild.id];
+//         if(!msg.guild.voiceConnection) return msg.say("I'm not connected to a Voice Channel!");
+//         if (!music || !music.playing) return msg.say("No music is playing!");
+//         msg.guild.voiceConnection.dispatcher.end("Skipped!");
+//         //console.log('Skipped!');
+//     }
+// };
