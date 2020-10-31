a15dc7c5de065b471ce5850d315de4c5b39d1b96
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-10-31 20:08:38 -0300

    Teams command now working properly with new discord intent

--- commands/shiloh/teams.js
+++ commands/shiloh/teams.js
@@ -1,3 +1,4 @@
+const { Collection } = require('discord.js');
 const { Command } = require('discord.js-commando');
 
 module.exports = class teamsCommand extends Command {
@@ -11,15 +12,20 @@ module.exports = class teamsCommand extends Command {
         });
     }
 
-    run(msg, args) {
+    async run(msg, args) {
        if (msg.guild.id !== process.env.SHILOH_SERVER_ID && msg.guild.id !== process.env.TEST_SERVER_ID) return;
 
        let teams_array = require('../../objects/roles_array');
-       let teams_size = [];
 
+       let shiloh_guild = await this.client.guilds.fetch(process.env.SHILOH_SERVER_ID);
+       if (!shiloh_guild.available) return msg.say("Coudn't count team members due to discord serve outage");
+
+       await shiloh_guild.members.fetch();
+
+       let teams_size = [];
+       
        teams_array.forEach(({id, names}) => {
-           let team_role = this.client.guilds.cache.get(process.env.SHILOH_SERVER_ID).roles.cache.get(id);
-           //if (!role || role.deleted) return;
+           let team_role = shiloh_guild.roles.resolve(id);
            if (!team_role) console.log(`Coudn't find role for ${names[0]} with id ${id}`);
            else if (team_role.deleted) console.log(`Role ${team_role.name} with id ${id} is deleted, skipping`);
            else teams_size.push({ name: team_role.name, size: team_role.members.size });
@@ -27,7 +33,7 @@ module.exports = class teamsCommand extends Command {
 
        if (teams_size.length === 0) return msg.say("Something went wrong. No roles found.");
 
-       teams_size.sort((a, b) => a.size > b.size ? -1 : a.size == b.size ? 0 : 1)
+       teams_size.sort((a, b) => b.size - a.size)
        let answer = teams_size.reduce((string, role) => string + role.name + ' - ' + role.size + '\n', 'Number of members in each team:\n\n');
        return msg.say(answer);
     }
