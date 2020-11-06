c53b45f21ae32af388ee4fbff35f8c6c824a2476
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-11-05 22:20:31 -0300

    checkStream function hopefully working with axios, old code just commented

--- custom_client.js
+++ custom_client.js
@@ -28,62 +28,93 @@ class Client extends Commando.CommandoClient {
 	    })
 	}
 
-	checkStream(handler, offline) {
-	    https.get('https://api.picarto.tv/v1/channel/name/REDnFLYNN', res => {
-	        const { statusCode } = res;
-	        const contentType = res.headers['content-type'];
+	async checkStream(handler, offline) {
+	    // https.get('https://api.picarto.tv/v1/channel/name/REDnFLYNN', res => {
+	    //     const { statusCode } = res;
+	    //     const contentType = res.headers['content-type'];
 
-	        let error;
-	        if (statusCode !== 200) {
-	            error = new Error('Picarto API: Request Failed.\n' +
-	            `Status Code: ${statusCode}`);
-	        } else if (!/^application\/json/.test(contentType)) {
-	            error = new Error('Picarto API: Invalid content-type.\n' +
-	            `Expected application/json but received ${contentType}`);
-	        }
-	        if (error) {
-	            console.error("Error with stream GET request: " + error.message);
-	            // consume response data to free up memory
-	            res.resume();
-	            return;
-	        }
+	    //     let error;
+	    //     if (statusCode !== 200) {
+	    //         error = new Error('Picarto API: Request Failed.\n' +
+	    //         `Status Code: ${statusCode}`);
+	    //     } else if (!/^application\/json/.test(contentType)) {
+	    //         error = new Error('Picarto API: Invalid content-type.\n' +
+	    //         `Expected application/json but received ${contentType}`);
+	    //     }
+	    //     if (error) {
+	    //         console.error("Error with stream GET request: " + error.message);
+	    //         // consume response data to free up memory
+	    //         res.resume();
+	    //         return;
+	    //     }
 
-	        res.setEncoding('utf8');
-	        let rawData = '';
-	        res.on('data', (chunk) => { rawData += chunk; });
-	        res.on('end', () => {
-	            try {
-	                const rnf = JSON.parse(rawData);
-	                if (handler.stream_status === null) {
-	                    handler.stream_status = rnf.online;
-	                    console.log("Stream status initialized as " + handler.stream_status);
-	                    return;
-	                }
-	                if (offline) {
-	                    handler.stream_status = rnf.online;
-	                    handler.stream_timer = setInterval(handler.checkStream, 30000);
-	                    if (!rnf.online) console.log("Stream is off");
-	                    return;
-	                }
-	                if (handler.stream_status === rnf.online) return;
-	                if (rnf.online) {
-	                    handler.stream_status = true;
-	                    handler.guilds.cache.get(process.env.SHILOH_SERVER_ID)
-	                        .channels.cache.get(process.env.SHILOH_CHANNEL_GENERAL)
-	                        .send(`${stream[Math.floor(Math.random() * stream.length)]} https://picarto.tv/REDnFLYNN`);
-	                    return;
-	                }
-	                clearInterval(handler.stream_timer);
-	                //console.log("CheckStream function");
-	                //console.log(handler.checkStream);
-	                setTimeout(handler.checkStream, 300000, true);
-	            } catch (e) {
-	                console.error("Error while parsing stream JSON: " + e.message);
-	            }
-	        });
-	    }).on('error', (e) => {
-	        console.error(`Error with stream GET response: ${e.message}`);
-	    });
+	    //     res.setEncoding('utf8');
+	    //     let rawData = '';
+	    //     res.on('data', (chunk) => { rawData += chunk; });
+	    //     res.on('end', () => {
+	    //         try {
+	    //             const rnf = JSON.parse(rawData);
+	    //             if (handler.stream_status === null) {
+	    //                 handler.stream_status = rnf.online;
+	    //                 console.log("Stream status initialized as " + handler.stream_status);
+	    //                 return;
+	    //             }
+	    //             if (offline) {
+	    //                 handler.stream_status = rnf.online;
+	    //                 handler.stream_timer = setInterval(handler.checkStream, 30000);
+	    //                 if (!rnf.online) console.log("Stream is off");
+	    //                 return;
+	    //             }
+	    //             if (handler.stream_status === rnf.online) return;
+	    //             if (rnf.online) {
+	    //                 handler.stream_status = true;
+	    //                 handler.guilds.cache.get(process.env.SHILOH_SERVER_ID)
+	    //                     .channels.cache.get(process.env.SHILOH_CHANNEL_GENERAL)
+	    //                     .send(`${stream[Math.floor(Math.random() * stream.length)]} https://picarto.tv/REDnFLYNN`);
+	    //                 return;
+	    //             }
+	    //             clearInterval(handler.stream_timer);
+	    //             //console.log("CheckStream function");
+	    //             //console.log(handler.checkStream);
+	    //             setTimeout(handler.checkStream, 300000, true);
+	    //         } catch (e) {
+	    //             console.error("Error while parsing stream JSON: " + e.message);
+	    //         }
+	    //     });
+	    // }).on('error', (e) => {
+	    //     console.error(`Error with stream GET response: ${e.message}`);
+		// });
+		try {
+			const answer = await axios.get('https://api.picarto.tv/v1/channel/name/REDnFLYNN');
+			const rnf = answer.data;
+			// console.log(rnf);
+            if (handler.stream_status === null) {
+				handler.stream_status = rnf.online;
+				console.log("Stream status initialized as " + handler.stream_status);
+				return;
+			}
+			if (offline) {
+				handler.stream_status = rnf.online;
+				handler.stream_timer = setInterval(handler.checkStream, 30000);
+				if (!rnf.online) console.log("Stream is off");
+				return;
+			}
+			if (handler.stream_status === rnf.online) return;
+			if (rnf.online) {
+				handler.stream_status = true;
+				handler.guilds.cache.get(process.env.SHILOH_SERVER_ID)
+					.channels.cache.get(process.env.SHILOH_CHANNEL_GENERAL)
+					.send(`${stream[Math.floor(Math.random() * stream.length)]} https://picarto.tv/REDnFLYNN`);
+				return;
+			}
+			clearInterval(handler.stream_timer);
+			//console.log("CheckStream function");
+			//console.log(handler.checkStream);
+			setTimeout(handler.checkStream, 300000, true);
+        } catch (e) {
+			console.error(`Error with Picarto API: ${e.message}`);
+			console.error(e);
+        }
 	}
 
 	async checkMoon() {
