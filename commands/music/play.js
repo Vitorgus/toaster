962dde7ffeeaa32f20ffac9ae16230000134ba09
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2020-11-08 19:08:09 -0300

    revamped play command

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,127 +1,65 @@
-// const { Command } = require('discord.js-commando');
+const { Command } = require('discord.js-commando');
 
-// const yt = require('ytdl-core');
-// var youtube_node = require('youtube-node');
-// youtube = new youtube_node();
-// youtube.setKey(process.env.TOKEN_YOUTUBE_API);
-// youtube.addParam('type', 'video');
+const ytdl = require('ytdl-core');
+const ytsearch = require("../../common/ytsearch");
 
-// /*
-// TODO find out why does jarvis won't play those videos
-// https://www.youtube.com/watch?v=L-u3fkgZkO0
-// https://www.youtube.com/watch?v=8GPo-6RQiN0
-// https://youtu.be/pSwD5vZ04iU
-// https://youtu.be/65BAeDpwzGY
-// */
+module.exports = class playCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'play',
+            group: 'music',
+            memberName: 'play',
+            description: 'Plays the given video.',
+            examples: ['jarvis play the sound of silence', 'jarvis play ptx', 'jarvis play http://www.youtube.com/watch?v=jvipPYFebWc'],
+            guildOnly: true,
+            args: [{
+                key: 'request',
+                prompt: 'What is the song you would like to play?',
+                type: 'string'
+            }]
+        });
+    }
 
-// // TODO checar detalhes de atribuição de variável em JS e node.js, pois isso pode explicar o porque do bug de música undefined 
+    async run(msg, { request }) {
 
-// module.exports = class playCommand extends Command {
-//     constructor(client) {
-//         super(client, {
-//             name: 'play',
-//             group: 'music',
-//             memberName: 'play',
-//             description: 'Plays the given video.',
-//             examples: ['jarvis play the sound of silence', 'jarvis play ptx', 'jarvis play http://www.youtube.com/watch?v=jvipPYFebWc'],
-//             guildOnly: true,
-//             args: [{
-//                 key: 'url',
-//                 prompt: 'What is the URL/tags of the video you would like to play?',
-//                 type: 'string'
-//             }]
-//         });
-//     }
+        if (!msg.member.voice.channel) {
+            return msg.reply('you have to be connected in a voice channel first!');
+        }
 
-//     async run(msg, { url }) {
-//         //msg.channel.startTyping();
-        
-//         const anchor = this;
-//         let music = this.client.music; //const?
+        let song = request;
 
-//         function queue(link) {
-//             yt.getInfo(link, (err, info) => {
-//                 if(err) {
-//                     //msg.channel.stopTyping();
-//                     return msg.say(`Whoops. Something went wrong with the song: \`${err}\``);
-//                 }
-//                 //TODO check somewhere in the code if the links starts with http://, cuz if not, add it in the beginning of it.
-//                 let song = {
-//                     url: link,
-//                     title: info.title,
-//                     user: msg.author.id
-//                 };
-//                 if (!music[msg.guild.id])
-//                     music[msg.guild.id] = {
-//                         playing: false,
-//                         queue: [],
-//                     }
-//                 if (!music[msg.guild.id].playing) {       //TODO find out why this is undefined
-//                     music[msg.guild.id].playing = true;
-//                     return play(song);
-//                 }
-//                 else {
-//                     music[msg.guild.id].queue.push(song);
-//                     //msg.channel.stopTyping();
-//                     return msg.say(`Added \`${info.title}\` to the queue for \`${msg.author.username}\``);
-//                 }
-//             });
-//         }
+        if (!song.startsWith('http')) {
+            try {
+                const id = await ytsearch(song);
+                if (!id) return msg.say(`Sorry ${msg.author}, but I didn't find any video when searching for \`${song}\``);
+                song = `http://www.youtube.com/watch?v=${id}`;
+            } catch (e) {
+                msg.reply(`something went wrong. Coudn't complete the search \`${song}\`: \`${e.message}\``)
+                console.log(e);
+            }
+        }
 
-//         async function play(song) {
-//             let channel = msg.member.voiceChannel;
-//             if (!song) {
-//                 msg.guild.voiceConnection.disconnect();//channel.leave();
-//                 music[msg.guild.id].playing = false;
-//                 console.log("Queue is empty!");
-//                 //msg.channel.stopTyping();
-//                 msg.say("Ended!");
-//                 return;
-//             }
-//             //TODO check if is not already connected to voiceChannel
-//             let connection = await channel.join();
-//             //.then(connection => {
-//             console.log(song.title + " - " + song.url);
-//             let stream = yt(song.url, {filter: 'audioonly'});
-//             let disp = connection.playStream(stream);
-//             disp.on('end', reason => {
-//                 reason ? console.log(reason) : console.log("Dispatcher ended");
-//                 //msg.channel.startTyping();
-//                 setTimeout(() => {
-//                     play(music[msg.guild.id].queue.shift());
-//                 }, 1000);
-//             });
-//             //msg.channel.stopTyping();
-//             return msg.say(`Now playing ${song.url} for <@${song.user}>`);
-//             //});
-//         }
+        let connection = null;
+        try {
+            connection = await msg.member.voice.channel.join();
 
-//         if (!msg.member.voiceChannel) {
-//             //msg.channel.stopTyping();
-//             return msg.reply(`Please be in a voice channel first!`);
-//         }
+            // TODO check some config on ytdl for opus stream, or check ytdl-core-discord
 
-//         //TODO replace the line below with regExp (Note: the link doesn't need http(s):// in front of it)
-//         if(url.startsWith("http://") || url.startsWith("https://")) return queue(url);
+            const dispatcher = connection.play(ytdl(song, {filter: 'audioonly'}));
+            dispatcher.on('finish', () => {
+                msg.say("Music ended!");
+                connection.disconnect();
+            });
 
-//         youtube.search(url, 1, {type: "video"}, function(error, result) {
-//             if (error)  {
-//                 //msg.channel.stopTyping();
-//                 return msg.say(`Error while searching for the video:  \`${error}\``);
-//             }
+            msg.say(`Now playing ${song} for ${msg.author}`);
+        } catch (e) {
+            msg.reply(`something went wrong. Coudn't keep playing the song \`${song}\``);
+            console.error(e);
 
-//             if (!result || !result.items || result.items.length < 1) {
-//                 console.log("ERROR_PLAY2");
-//                 console.log("Result: " + result);
-//                 console.log("Result items: " + result.items);
-//                 console.log("Result items length " + result.items.length);
-//                 //msg.channel.stopTyping();
-//                 return msg.say(`Something that couldn't go wrong, went wrong. ${anchor.client.owners[0]}, check the logs.`);
-//             } else {
-//                 let final_url = "http://www.youtube.com/watch?v=" + result.items[0].id.videoId;
-//                 return queue(final_url);
-//             }
-//         });
-//     }
+            if (connection) {
+                connection.disconnect();
+            }
+        }
+    }
     
-// };
+};
